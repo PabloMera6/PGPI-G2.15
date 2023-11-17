c005c84861e3601cfefadd17077ba5d1e53242e1
Alejandrocg024 <alecamgal1@alum.us.es>
2023-11-17 16:25:21 +0100

    default

--- src/product/models.py
+++ src/product/models.py
@@ -11,4 +11,4 @@ class Product(models.Model):
     reference_number =  models.CharField(max_length=15,unique=True)
     product_type = models.CharField(max_length=1,choices=TYPE_CHOICES)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name='manufacturer')
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, default=None, related_name='manufacturer')
