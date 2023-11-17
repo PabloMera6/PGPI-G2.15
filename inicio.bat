980f8e2a06506003c23c8d8173bf147163126c99
Alejandrocg024 <alecamgal1@alum.us.es>
2023-11-17 17:07:10 +0100

    inicio automatizado

---
+++ inicio.bat
@@ -0,0 +1,8 @@
+cd ./src
+pip install -r requirements.txt
+python ./manage.py makemigrations
+python ./manage.py migrate --run-syncdb
+python ./manage.py loaddata populate.json
+coverage run manage.py test 
+coverage report -m
+python ./manage.py runserver
