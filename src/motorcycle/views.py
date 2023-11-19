36c4c9350657da51b0c41b2364101e73ed1be31c
davcorrom <davcorrom@alum.us.es>
2023-11-19 13:24:34 +0100

    feat Paginación en visualización

--- src/motorcycle/views.py
+++ src/motorcycle/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from motorcycle.models import Motorcycle
 
 def view_motorcycles(request):
-    motorcycles = Motorcycle.objects.all()
+    motorcycles_list = Motorcycle.objects.all()
+
+    motorcycles_per_page = 12
+    page = request.GET.get('page')
+    paginator = Paginator(motorcycles_list, motorcycles_per_page)
+
+    try:
+        motorcycles = paginator.page(page)
+    except PageNotAnInteger:
+        motorcycles = paginator.page(1)
+    except EmptyPage:
+        motorcycles = paginator.page(paginator.num_pages)
+
     return render(request, 'motorcycles.html', {'motorcycles': motorcycles})
