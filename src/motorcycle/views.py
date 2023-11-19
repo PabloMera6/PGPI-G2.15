53dadc2f90b99f98c2fcd7187ab81a6566fdbf2a
davcorrom <davcorrom@alum.us.es>
2023-11-19 12:48:16 +0100

    feat Visualización de motocicletas

--- src/motorcycle/views.py
+++ src/motorcycle/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from motorcycle.models import Motorcycle
 
-# Create your views here.
+def view_motorcycles(request):
+    motorcycles = Motorcycle.objects.all()
+    return render(request, 'motorcycles.html', {'motorcycles': motorcycles})
