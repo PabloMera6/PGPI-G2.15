a4cb97f885b14935188274bd2cccf69522e45f2c
davcorrom <davcorrom@alum.us.es>
2023-11-20 18:09:13 +0100

    feat Visualización detalles de fabricante

--- src/manufacturer/urls.py
+++ src/manufacturer/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('manufacturers/', views.view_manufacturers, name='manufacturers'),
+    path('manufacturers/<int:manufacturer_id>/', views.view_manufacturer_details, name='manufacturer_details')
 ]
