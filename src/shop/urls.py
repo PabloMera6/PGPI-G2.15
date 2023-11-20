312fc2a1d112dd035360df8928b911c27373ea3b
Alejandrocg024 <alecamgal1@alum.us.es>
2023-11-20 10:11:18 +0100

    cesta de la compra done fixes #36

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.welcome, name='initial'),
     path('cart/', views.view_cart, name='cart'),
+    path('add_to_cart/', views.add_to_cart, name='add_to_cart'),
+    path('cart/remove/', views.remove_cart, name='cart_remove'),
+    path('cart/refresh/', views.refresh, name='refresh'),
 ]
