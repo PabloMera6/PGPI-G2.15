e693e77180943dbb80039758daba9d7fe156b226
PabloMera6 <pabmergom@alum.us.es>
2023-11-22 22:53:59 +0100

    feat seguimiento

--- src/motorcycle/urls.py
+++ src/motorcycle/urls.py
@@ -1,9 +1,13 @@
 from django import views
 from django.urls import path
 from . import views
+from order.views import check_order_status
+
 
 
 urlpatterns = [
     path('motorcycles/', views.view_motorcycles, name='motorcycles'),
-    path('motorcycles/<int:motorcycle_id>/', views.view_motorcycle_details, name='motorcycle_details')
+    path('motorcycles/<int:motorcycle_id>/', views.view_motorcycle_details, name='motorcycle_details'),
+    path('check_order_status/', check_order_status, name='check_order_status'),
+
 ]
--- src/order/views.py
+++ src/order/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Order
+from django.contrib import messages
 
 def user_orders(request):
     # Asegúrate de que el usuario esté autenticado
@@ -11,3 +12,18 @@ def user_orders(request):
     else:
         # Si el usuario no está autenticado, redirígelos a la página de inicio de sesión
         return redirect('login')  # Ajusta esto según tu URL de inicio de sesión
+    
+
+def check_order_status(request):
+    if request.method == 'POST':
+        order_id = request.POST.get('order_id')
+        try:
+            confirm_url = get_confirm_url(int(order_id))
+            return redirect(confirm_url)
+        except Order.DoesNotExist:
+            messages.error(request, 'El ID de pedido ingresado no existe.')
+    
+    return render(request, 'check_order_status.html')
+
+def get_confirm_url(order_id):
+    return f'/checkout/confirm/confirmed/{order_id}/'
