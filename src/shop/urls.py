7dee4d547ae1b4896d28c2606c0cc3cfbab4b2c7
Juan Jesús Campos Garrido <guaje3181@gmail.com>
2023-11-26 18:47:51 +0100

    Añadida gestión de pedidos para el administrador

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -1,6 +1,9 @@
 from django import views
 from django.urls import path
 from . import views
+from order.views import administrate,orders,administrate_order
+from manufacturer.models import Manufacturer
+
 
 
 urlpatterns = [
@@ -12,5 +15,8 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     path('checkout/', views.checkout, name='checkout'),
     path('checkout/confirm/', views.confirm, name='confirm'),
-    path('checkout/confirm/confirmed/<int:order_id>/', views.confirmed, name='confirmed'),
+    path('checkout/confirm/confirmed/<uuid:order_id>/', views.confirmed, name='confirmed'),
+    path('administrate/', administrate, name='administrate'),
+    path('administrate/orders/',orders, name='orders'),
+    path('administrate/orders/<uuid:order_id>/', administrate_order, name='administrate_order'),
 ]
