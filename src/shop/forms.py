5ccca36ed96a67ec30436db985d774c5a0e50319
antcardia <antcardia4@alum.us.es>
2023-11-26 18:19:29 +0100

    Paginación arreglada y añadido filtro busqueda por precio

--- src/shop/forms.py
+++ src/shop/forms.py
@@ -4,3 +4,13 @@ class SearchForm(forms.Form):
     query = forms.CharField(label='Buscar productos', max_length=100)
     search_type = forms.ChoiceField(choices=[('all', 'Todos'), ('motorcycles', 'Motocicletas'), ('parts', 'Partes'), ('manufacturers', 'Fabricantes')],
                                    initial='all', widget=forms.Select(attrs={'class': 'form-control'}))
+    min_price = forms.DecimalField(
+        label='Precio mínimo',
+        required=False,
+        widget=forms.NumberInput(attrs={'class': 'form-control'})
+    )
+    max_price = forms.DecimalField(
+        label='Precio máximo',
+        required=False,
+        widget=forms.NumberInput(attrs={'class': 'form-control'})
+    )
