dbcef6693c24468752f27330b67b167f6f876a82
PabloMera6 <pabmergom@alum.us.es>
2023-11-28 18:56:37 +0100

    fix claims

--- src/claim/models.py
+++ src/claim/models.py
@@ -11,6 +11,7 @@ class Claim(models.Model):
     id = models.AutoField(primary_key=True)
     author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
+    title = models.TextField(max_length=100, default="Sin título")
     description = models.TextField(max_length=500)
     response = models.TextField(max_length=500, blank=True, null=True)
 
