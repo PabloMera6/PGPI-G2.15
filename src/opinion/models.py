052ff5d9a4e8bc8b48dd6068a755c86a8ab21f04
davcorrom <davcorrom@alum.us.es>
2023-11-28 16:47:45 +0100

    feat Realizar opiniones

--- src/opinion/models.py
+++ src/opinion/models.py
@@ -3,11 +3,10 @@ from users.models import UserProfile
 from product.models import Product
 
 
-# Create your models here.
 class Opinion(models.Model):
     id = models.AutoField(primary_key=True)
     author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
-    score = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
-    description = models.TextField(max_length=500)
+    score = models.IntegerField(choices=[(i, i) for i in range(1, 6)], blank=False, null=False)
+    description = models.TextField(max_length=500, null=False, blank=False)
