55e539b35c51c9ad2f100a0666e6fe0fb70f2b71
Alejandrocg024 <alecamgal1@alum.us.es>
2023-11-28 21:07:04 +0100

    ultimos cambios

--- src/motorcycle/static/styles/motorcycle_details.js
+++ src/motorcycle/static/styles/motorcycle_details.js
@@ -31,3 +31,12 @@ function validateQuantity(input) {
         input.value = maxStock;
     }
 }
+
+$(function(){
+	$('div.product-chooser').not('.disabled').find('div.product-chooser-item').on('click', function(){
+		$(this).parent().parent().find('div.product-chooser-item').removeClass('selected');
+		$(this).addClass('selected');
+		$(this).find('input[type="radio"]').prop("checked", true);
+		
+	});
+});
--- src/motorcycle/views.py
+++ src/motorcycle/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from part.models import Part
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from motorcycle.models import Motorcycle
+from motorcycle.models import Motorcycle, DerivedMotorcycle
 from product.models import Product
+from shop.views import Cart, view_cart
+from django.contrib import messages
 
 def view_motorcycles(request):
     motorcycles_list = Motorcycle.objects.all()
@@ -48,3 +51,53 @@ def view_motorcycle_details(request, motorcycle_id):
     )
 
     return render(request, 'motorcycle_detail.html', {'motorcycle': motorcycle, 'product': product, 'compatible_parts': compatible_parts,'selected_parts': selected_parts})
+
+
+def config(request, motorcycle_id):
+    product = get_object_or_404(Product, pk=motorcycle_id)
+    motorcycle = get_object_or_404(Motorcycle, pk=motorcycle_id)
+    if request.method == 'POST':
+        my_cart = Cart(request)
+        carroceria =   int(request.POST.get('carroceria'))
+        motor =  int(request.POST.get('motor'))
+        transmision = int(request.POST.get('transmision'))
+        suspension = int(request.POST.get('suspension'))
+        ruedas = int(request.POST.get('ruedas'))
+        frenos = int(request.POST.get('frenos'))
+        manillar = int(request.POST.get('manillar'))
+        combustible = int(request.POST.get('combustible'))
+        chasis = int(request.POST.get('chasis'))
+        my_moto = DerivedMotorcycle(
+            base_motorcycle_id=motorcycle_id,
+            carroceria_id=carroceria,
+            motor_id=motor,
+            transmision_id=transmision,
+            suspension_id=suspension,
+            ruedas_id=ruedas,
+            frenos_id=frenos,
+            manillar_id=manillar,
+            combustible_id=combustible,
+            chasis_id=chasis,
+            manufacturer_id=product.manufacturer_id,
+            reference_number=product.reference_number,
+            product_type=product.product_type,
+        )
+        my_moto.save() 
+        my_cart.add(my_moto.id, 1)  
+        return view_cart(request)
+    else:
+        compatible_parts = (
+            motorcycle.compatible_carroceria.all() |
+            motorcycle.compatible_motor.all() |
+            motorcycle.compatible_transmision.all() |
+            motorcycle.compatible_suspension.all() |
+            motorcycle.compatible_ruedas.all() |
+            motorcycle.compatible_frenos.all() |
+            motorcycle.compatible_manillar.all() |
+            motorcycle.compatible_combustible.all() |
+            motorcycle.compatible_chasis.all()
+        ).distinct()
+
+
+
+        return render(request, 'motorcycle_config.html', {'motorcycle': motorcycle, 'product': product, 'compatible_parts': compatible_parts})
