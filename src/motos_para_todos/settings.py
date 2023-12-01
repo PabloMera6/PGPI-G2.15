016640f2b2300cb6cbe4debe217e77a96bb4f118
Juan Jesús Campos Garrido <guaje3181@gmail.com>
2023-12-01 23:14:29 +0100

    q

--- src/motos_para_todos/settings.py
+++ src/motos_para_todos/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = 'static/'
 #STATIC_ROOT = 'app/PGPI-G2.15/src/static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
