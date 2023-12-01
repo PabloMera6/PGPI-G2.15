b9309cf72aa4b2609105c5b4b504fb355059b39c
Juan Jesús Campos Garrido <guaje3181@gmail.com>
2023-12-01 20:13:30 +0100

    dockerfile

--- inicio.bat
+++ inicio.bat
@@ -1,6 +1,5 @@
 cd ./src
 pip install -r requirements.txt
-python ./manage.py makemigrations
 python ./manage.py migrate --run-syncdb
 python ./manage.py loaddata populate.json
 coverage run manage.py test 
--- src/motos_para_todos/settings.py
+++ src/motos_para_todos/settings.py
@@ -17,6 +17,9 @@ import paypalrestsdk
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(app, 'static') for app in BASE_DIR]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
