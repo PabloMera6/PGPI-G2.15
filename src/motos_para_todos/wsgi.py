ae981ebbf4364a7b713d83b269cac6382766bd47
Juan Jesús Campos Garrido <guaje3181@gmail.com>
2023-12-01 23:31:29 +0100

    r

--- src/motos_para_todos/wsgi.py
+++ src/motos_para_todos/wsgi.py
@@ -10,9 +10,13 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from django.conf import settings
+BASE_DIR = settings.BASE_DIR
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "motos_para_todos.settings")
 
 application = get_wsgi_application()
 
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
 
