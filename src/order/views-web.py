df1392ad87cbe02da026c0dc0d104619d22665bd
Alejandrocg024 <alecamgal1@alum.us.es>
2023-12-03 03:44:30 +0100

    despliegue web

--- src/order/views-web.py
+++ src/order/views-web.py
@@ -12,9 +12,9 @@ from motorcycle.models import Motorcycle
 from part.models import Part
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+# import matplotlib
+# matplotlib.use('Agg')
+# import matplotlib.pyplot as plt
 
 def user_orders(request):
 
@@ -136,9 +136,9 @@ def administrate_sells(request):
             ciudad_mas_ventas = obtener_ciudad_mas_ventas()
             forma_pago_preferida = obtener_forma_pago_preferida()
             cliente_mas_activo = obtener_cliente_mas_activo()
-            grafica_ventas_por_producto = generar_grafica_ventas_por_producto()
-            grafica_ventas_por_tipo = generar_grafica_ventas_por_tipo()
-            grafica_pedidos_por_estado = generar_grafica_pedidos_por_estado()
+            # grafica_ventas_por_producto = generar_grafica_ventas_por_producto()
+            # grafica_ventas_por_tipo = generar_grafica_ventas_por_tipo()
+            # grafica_pedidos_por_estado = generar_grafica_pedidos_por_estado()
         return render(request, 'administrate_sells.html', {
         'producto_mas_vendido': producto_mas_vendido,
         'media_precios': media_precios,
@@ -148,9 +148,9 @@ def administrate_sells(request):
         'ciudad_mas_ventas': ciudad_mas_ventas,
         'forma_pago_preferida': forma_pago_preferida,
         'cliente_mas_activo': cliente_mas_activo,
-        'grafica_ventas_por_producto': grafica_ventas_por_producto,
-        'grafica_ventas_por_tipo': grafica_ventas_por_tipo,
-        'grafica_pedidos_por_estado': grafica_pedidos_por_estado,
+        # 'grafica_ventas_por_producto': grafica_ventas_por_producto,
+        # 'grafica_ventas_por_tipo': grafica_ventas_por_tipo,
+        # 'grafica_pedidos_por_estado': grafica_pedidos_por_estado,
     })
     else:
         return redirect('/')
@@ -299,108 +299,108 @@ def obtener_cliente_mas_activo():
             max_order = order
     return max_order
 
-def generar_grafica_ventas_por_producto():
-    orders = Order.objects.all()
-    order_products = OrderProduct.objects.all()
-    products = Product.objects.all()
-    product_count = {}
+# def generar_grafica_ventas_por_producto():
+#     orders = Order.objects.all()
+#     order_products = OrderProduct.objects.all()
+#     products = Product.objects.all()
+#     product_count = {}
 
-    for product in products:
-        product_count[product] = 0
+#     for product in products:
+#         product_count[product] = 0
 
-    for order in orders:
-        for order_product in order_products:
-            if order_product.order == order:
-                product_count[order_product.product] += order_product.quantity
+#     for order in orders:
+#         for order_product in order_products:
+#             if order_product.order == order:
+#                 product_count[order_product.product] += order_product.quantity
 
-    labels = []
-    sizes = []
+#     labels = []
+#     sizes = []
 
-    for product in product_count:
-        if product.product_type == "M":
-            moto = Motorcycle.objects.get(id=product.id)
-            name = moto.name
-        else:
-            part = Part.objects.get(id=product.id)
-            name = part.name
+#     for product in product_count:
+#         if product.product_type == "M":
+#             moto = Motorcycle.objects.get(id=product.id)
+#             name = moto.name
+#         else:
+#             part = Part.objects.get(id=product.id)
+#             name = part.name
 
-        if product_count[product] > 0:
-            labels.append(name)
-            sizes.append(product_count[product])
+#         if product_count[product] > 0:
+#             labels.append(name)
+#             sizes.append(product_count[product])
 
-    fig, ax = plt.subplots()
-    bars = ax.bar(labels, sizes, color='blue')
-    for bar, size in zip(bars, sizes):
-        height = bar.get_height()
-        ax.annotate(f'{size}',
-                    xy=(bar.get_x() + bar.get_width() / 2, height),
-                    xytext=(0, 3),
-                    textcoords="offset points",
-                    ha='center', va='bottom')
+#     fig, ax = plt.subplots()
+#     bars = ax.bar(labels, sizes, color='blue')
+#     for bar, size in zip(bars, sizes):
+#         height = bar.get_height()
+#         ax.annotate(f'{size}',
+#                     xy=(bar.get_x() + bar.get_width() / 2, height),
+#                     xytext=(0, 3),
+#                     textcoords="offset points",
+#                     ha='center', va='bottom')
     
-    ax.set_title("Ventas por producto")
-    ax.set_xlabel("Productos")
-    ax.set_ylabel("Cantidad vendida")
-    plt.savefig('order/static/images/ventas_por_producto.png')
-    plt.close()
+#     ax.set_title("Ventas por producto")
+#     ax.set_xlabel("Productos")
+#     ax.set_ylabel("Cantidad vendida")
+#     plt.savefig('order/static/images/ventas_por_producto.png')
+#     plt.close()
     
 
-def generar_grafica_ventas_por_tipo():
-    orders = Order.objects.all()
-    order_products = OrderProduct.objects.all()
-    products = Product.objects.all()
-    product_count = {}
-    for product in products:
-        product_count[product] = 0
-    for order in orders:
-        for order_product in order_products:
-            if order_product.order == order:
-                product_count[order_product.product] += order_product.quantity
-    motos_count = 0
-    parts_count = 0
-    for product in product_count:
-        if product.product_type == "M":
-            motos_count += product_count[product]
-        else:
-            parts_count += product_count[product]
-    labels = 'Motocicletas', 'Partes'
-    sizes = [motos_count, parts_count]
-    fig1, ax1 = plt.subplots()
-    wedges, texts, autotexts = ax1.pie(
-        sizes,
-        labels=labels,
-        autopct='%1.1f%%',
-        startangle=90,
-        textprops=dict(color="black")
-    )
-    legend_labels = [f'{label} ({size})' for label, size in zip(labels, sizes)]
-    ax1.legend(wedges, legend_labels, title="Tipos de Producto", loc="center", bbox_to_anchor=(0.7, 0.5, 0.5, 1))
-    ax1.axis('equal')
-    plt.savefig('order/static/images/ventas_por_tipo.png')
-    plt.close()
+# def generar_grafica_ventas_por_tipo():
+#     orders = Order.objects.all()
+#     order_products = OrderProduct.objects.all()
+#     products = Product.objects.all()
+#     product_count = {}
+#     for product in products:
+#         product_count[product] = 0
+#     for order in orders:
+#         for order_product in order_products:
+#             if order_product.order == order:
+#                 product_count[order_product.product] += order_product.quantity
+#     motos_count = 0
+#     parts_count = 0
+#     for product in product_count:
+#         if product.product_type == "M":
+#             motos_count += product_count[product]
+#         else:
+#             parts_count += product_count[product]
+#     labels = 'Motocicletas', 'Partes'
+#     sizes = [motos_count, parts_count]
+#     fig1, ax1 = plt.subplots()
+#     wedges, texts, autotexts = ax1.pie(
+#         sizes,
+#         labels=labels,
+#         autopct='%1.1f%%',
+#         startangle=90,
+#         textprops=dict(color="black")
+#     )
+#     legend_labels = [f'{label} ({size})' for label, size in zip(labels, sizes)]
+#     ax1.legend(wedges, legend_labels, title="Tipos de Producto", loc="center", bbox_to_anchor=(0.7, 0.5, 0.5, 1))
+#     ax1.axis('equal')
+#     plt.savefig('order/static/images/ventas_por_tipo.png')
+#     plt.close()
 
-def generar_grafica_pedidos_por_estado():
-    orders = Order.objects.all()
-    order_count = {}
-    for order in orders:
-        order_count[order.state] = 0
-    for order in orders:
-        order_count[order.state] += 1
-    labels = []
-    sizes = []
-    for order in order_count:
-        labels.append(order)
-        sizes.append(order_count[order])
-    fig1, ax1 = plt.subplots()
-    wedges, texts, autotexts = ax1.pie(
-        sizes,
-        labels=labels,
-        autopct='%1.1f%%',
-        startangle=90,
-        textprops=dict(color="black")
-    )
-    legend_labels = [f'{label} ({size})' for label, size in zip(labels, sizes)]
-    ax1.legend(wedges, legend_labels, title="Estados de Pedido", loc="center", bbox_to_anchor=(0.7, 0.5, 0.5, 1))
-    ax1.axis('equal')
-    plt.savefig('order/static/images/pedidos_por_estado.png')
-    plt.close()
+# def generar_grafica_pedidos_por_estado():
+#     orders = Order.objects.all()
+#     order_count = {}
+#     for order in orders:
+#         order_count[order.state] = 0
+#     for order in orders:
+#         order_count[order.state] += 1
+#     labels = []
+#     sizes = []
+#     for order in order_count:
+#         labels.append(order)
+#         sizes.append(order_count[order])
+#     fig1, ax1 = plt.subplots()
+#     wedges, texts, autotexts = ax1.pie(
+#         sizes,
+#         labels=labels,
+#         autopct='%1.1f%%',
+#         startangle=90,
+#         textprops=dict(color="black")
+#     )
+#     legend_labels = [f'{label} ({size})' for label, size in zip(labels, sizes)]
+#     ax1.legend(wedges, legend_labels, title="Estados de Pedido", loc="center", bbox_to_anchor=(0.7, 0.5, 0.5, 1))
+#     ax1.axis('equal')
+#     plt.savefig('order/static/images/pedidos_por_estado.png')
+#     plt.close()
