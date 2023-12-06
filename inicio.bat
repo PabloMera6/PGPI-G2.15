7e9ec1ae1a3a910f3519f2a5355c5a23b8d7aff6
Alejandrocg024 <alecamgal1@alum.us.es>
2023-12-06 12:37:36 +0100

    update web

--- inicio.bat
+++ inicio.bat
@@ -2,4 +2,4 @@ cd ./src
 pip install -r requirements.txt
 python ./manage.py migrate --run-syncdb
 python ./manage.py loaddata populate.json
-python ./manage.py runserver
+python ./manage.py runserver 8001
