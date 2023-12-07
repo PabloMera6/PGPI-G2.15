317be003264e276369f790ebf482c8d497caa974
PabloMera6 <pabmergom@alum.us.es>
2023-12-07 18:34:49 +0100

    cambio correo

--- src/motos_para_todos/settings.py
+++ src/motos_para_todos/settings.py
@@ -96,11 +96,11 @@ TEMPLATES = [
 WSGI_APPLICATION = "motos_para_todos.wsgi.application"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'motosparatodos@outlook.es'
-EMAIL_HOST_PASSWORD = 'CampanoParienta'
+EMAIL_HOST_USER = 'motosparatodos2@gmail.com'
+EMAIL_HOST_PASSWORD = 'uzjy mibt rwio ayuz'
 
 
 # Database
